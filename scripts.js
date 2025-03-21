85dc67955f20a83b19eb2f289d76e33b374d157d
Rodrigo Martins <mr.rodrigocarrico@gmail.com>
2025-03-21 10:39:00 +0000

    catching coin value by submit

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 // Obter os elementos do form
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
 //Manipular o input amount para receber somente números
 amount.addEventListener("input", ()=>{
@@ -7,6 +9,12 @@ amount.addEventListener("input", ()=>{
   amount.value = amount.value.replace(hasCharactersRegex, "")
 })
 
+
+// Capturar o evento de submit(enviar) do formulário
+form.onsubmit = (event) => {
+  event.preventDefault()
+  console.log(currency.value)
+}
 
 
 
